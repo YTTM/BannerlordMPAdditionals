d35db7508ee3972fd97f2f0cb432646ddd8fc096
YTTM <120769366+YTTM@users.noreply.github.com>
2023-01-15 00:54:56 +0100

    add Morale class for fixed siege morale

---
+++ Morale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace BannerlordMPAdditionals
+{
+    class PatchMissionMultiplayerSiegeGetMoraleGain
+    {
+        public static void GetMoraleGain0_Postfix(ref int __result)
+        {
+            __result = 0;
+        }
+    }
+}
